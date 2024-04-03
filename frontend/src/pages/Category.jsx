fd453b51664129a951983e470abfdd838f943774
Momanyi Poet <momanyiturner@gmail.com>
2024-04-03 13:06:18 +0300

    added async

--- frontend/src/pages/Category.jsx
+++ frontend/src/pages/Category.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { Helmet } from 'react-helmet';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 import Header from '../components/Header';
 import Footer from '../components/Footer';
@@ -16,11 +16,13 @@ const Category = () => {
 
   return (
     <>
-        <Helmet>
-          <meta property="og:image" content="https://minty-body-glamour.vercel.app/assets/bag2-NNQiZTyr.webp"/>
-          <meta property="og:title" content="MINTY | BEST | AFFORDABLE | FASHION | BAGS" />
-          <meta property="og:description" content="Explore our fashionable bags and meet your aspirations with Us! MINTY, Connecting new looks and design with enthusiastic lovers." />
-        </Helmet>
+        <HelmetProvider>
+            <Helmet defer={false}>
+                <meta property="og:image" content="https://minty-body-glamour.vercel.app/assets/bag2-NNQiZTyr.webp"/>
+                <meta property="og:title" content="APEX Bag" />
+                <meta property="og:description" content="Explore our fashionable bags and meet your aspirations with Us! MINTY, Connecting new looks and design with enthusiastic lovers." />
+            </Helmet>
+        </HelmetProvider>
 
         <Header />
 
